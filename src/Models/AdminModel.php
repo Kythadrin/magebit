3e5171d6d2b253ba9185d5a7fdaad070a779470b
Kythadrin <vadimsiric@gmail.com>
2021-12-24 19:24:53 +0200

    clearing

--- src/Models/AdminModel.php
+++ src/Models/AdminModel.php
@@ -4,10 +4,11 @@ class AdminModel extends Model
 {
     public function getEmails($email, $sort, $filter) 
     {
-        $sql = "SELECT * FROM `subscriptions` WHERE email LIKE :email AND SUBSTR(email, INSTR(email, '@') + 1) LIKE '$filter' ORDER BY $sort";
+        $sql = "SELECT * FROM `subscriptions` WHERE email LIKE :email AND SUBSTR(email, INSTR(email, '@') + 1) LIKE :filter ORDER BY $sort";
         $stmt = $this->db->prepare($sql);
 
         $stmt->bindParam('email', $email, PDO::PARAM_STR);
+        $stmt->bindParam('filter', $filter, PDO::PARAM_STR);
         
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -15,7 +16,7 @@ class AdminModel extends Model
 
     public function getDomains()
     {
-        $sql = "SELECT SUBSTR(email, INSTR(email, '@') + 1) as 'domain' FROM `subscriptions` group by SUBSTR(email, INSTR(email, '@') + 1)";
+        $sql = "SELECT SUBSTR(email, INSTR(email, '@') + 1) as 'domain' FROM `subscriptions` GROUP BY SUBSTR(email, INSTR(email, '@') + 1)";
         $stmt = $this->db->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
